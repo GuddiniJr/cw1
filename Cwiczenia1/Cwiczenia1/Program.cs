1731311bf9de65a8f01e461e25c632f268f69346
Maksym Alvan <s18511@pjwstk.edu.pl>
2020-03-02 11:30:43 +0100

    Some message

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cwiczenia1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+sad
